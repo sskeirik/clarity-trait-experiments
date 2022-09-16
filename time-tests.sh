b0d8cafc8b26baa11514d0940e400326cafd3def
Stephen Skeirik <stephen.skeirik@runtimeverification.com>
2022-09-16 12:55:31 -0400

    export timing data as tab-separated values

--- time-tests.sh
+++ time-tests.sh
@@ -2,4 +2,6 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-$SCRIPT_DIR/run-tests.sh time 2>&1 | grep '^>>> ' | cut -c 5- | awk 'NR%2{printf "%s ",$0;next;}1'
+val=$($SCRIPT_DIR/run-tests.sh time 2>&1 | grep '^>>> ' | cut -c 5- | awk 'NR%2{printf "%s\t",$0;next;}1')
+echo -e "time\tstatus\tname"
+echo "$val"
